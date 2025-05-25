f5174b6eaba6d3820a602607789bee373641db3c
Nisarg Desai <nisarg.desai599@gmail.com>
2025-05-25 14:01:18 -0700

    updates

--- init.lua
+++ init.lua
@@ -2,4 +2,15 @@ require("neeskebabs.remap")
 require("neeskebabs.packer")
 print("hello")
 
+-- basic line number options
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- cursor options
+vim.opt.cursorline = true
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
 
